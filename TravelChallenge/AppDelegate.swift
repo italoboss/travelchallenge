ea99230c952ac4c4ac0b078a87ccc0347b49c6c0
jullynobre <jullynobre1@gmail.com>
2018-09-03 12:18:26 -0300

    Trigger notification in appDelegate

--- TravelChallenge/AppDelegate.swift
+++ TravelChallenge/AppDelegate.swift
@@ -31,6 +31,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
             print("Granted: \(granted)")
         }
+        NotificationController().triggerNofication()
         return true
     }
 
