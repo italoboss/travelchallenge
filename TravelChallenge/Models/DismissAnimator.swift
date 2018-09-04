5f59659f0ecdaf0b74ad2679985f6eb132f8ffd0
jullynobre <jullynobre1@gmail.com>
2018-09-04 11:01:50 -0300

    Add DismissAnimator and Interactor

---
+++ TravelChallenge/Models/DismissAnimator.swift
@@ -0,0 +1,48 @@
+//
+//  DismissAnimator.swift
+//  TravelChallenge
+//
+//  Created by Ada 2018 on 04/09/18.
+//  Copyright © 2018 Dinamite Pangalactica. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DismissAnimator: NSObject {
+    
+}
+
+extension DismissAnimator: UIViewControllerAnimatedTransitioning{
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        guard
+            let fromVC = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from),
+            let toVC = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to)
+        else {
+            return
+        }
+        
+        let containerView = transitionContext.containerView
+        containerView.insertSubview(toVC.view, belowSubview: fromVC.view)
+        
+        let screenBounds = UIScreen.main.bounds
+        let bottomLeftCorner = CGPoint(x: 0, y: screenBounds.height)
+        let finalFrame = CGRect(origin: bottomLeftCorner, size: screenBounds.size)
+        
+        UIView.animate(
+            withDuration: transitionDuration(using: transitionContext),
+            animations: {
+                fromVC.view.frame = finalFrame
+        },
+            completion: { _ in
+                transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+            }
+        )
+    }
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 3.0
+    }
+    
+}
---
+++ TravelChallenge/Models/Interactor.swift
@@ -0,0 +1,15 @@
+//
+//  Interaction.swift
+//  TravelChallenge
+//
+//  Created by Ada 2018 on 04/09/18.
+//  Copyright © 2018 Dinamite Pangalactica. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Interaction: UIPercentDrivenInteractiveTransition {
+    var hasStartd = false
+    var shouldFinish = false
+}
